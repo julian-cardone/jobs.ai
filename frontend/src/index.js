48bae30ed5ddad75de1599408b7fe0628cd1c229
Julian Cardone <juliancardone1@gmail.com>
2023-03-20 22:56:32 -0400

    setup react app

---
+++ frontend/src/index.js
@@ -0,0 +1,31 @@
+// src/index.js
+
+import React from 'react';
+import './index.css';
+import "./reset.css";
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store/store';
+import { createRoot } from 'react-dom/client';
+
+
+let store = configureStore({});
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+root.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
