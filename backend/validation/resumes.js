c6dfb35c4a262a8c61daf84f3a9608c0e6c9bdd2
Julian Cardone <juliancardone1@gmail.com>
2023-03-22 17:46:24 -0400

    resume rest functionality is working

--- backend/validation/resumes.js
+++ backend/validation/resumes.js
@@ -1,20 +1,20 @@
-const Validator = require('validator');
+const Validator = require("validator");
 
 module.exports = function validateResumeInput(data) {
   let errors = {};
 
-  // data.text = validTitle(data.title) ? data.title : '';
+  // data.title = validText(data.title) ? data.title : '';
 
   if (!Validator.isLength(data.title, { min: 0, max: 50 })) {
-    errors.text = 'Title must be between 0 and 50 characters';
+    errors.title = "Title must be between 0 and 50 characters";
   }
 
   if (Validator.isEmpty(data.file)) {
-    errors.text = 'Resume file is required';
+    errors.file = "Resume file is required";
   }
 
   return {
     errors,
-    isValid: Object.keys(errors).length === 0
+    isValid: Object.keys(errors).length === 0,
   };
 };
