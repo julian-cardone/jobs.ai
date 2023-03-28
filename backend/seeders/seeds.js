4eba2ec7745199f9d464c09acb1ab4ed9eba9771
Julian Cardone <juliancardone1@gmail.com>
2023-03-28 18:28:42 -0400

    add protected routes

---
+++ backend/seeders/seeds.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const { mongoURI: db } = require('../config/keys.js');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+// Create users
+const users = [];
+
+users.push(
+  new User ({
+    username: 'demo-user',
+    email: 'demo-user@appacademy.io',
+    hashedPassword: bcrypt.hashSync('starwars', 10)
+  })
+)
+
+const insertSeeds = () => {
+  console.log("Resetting db and seeding users...");
+
+  User.collection.drop()
+                 .then(() => User.insertMany(users))
+                 .then(() => {
+                   console.log("Done!");
+                   mongoose.disconnect();
+                 })
+                 .catch(err => {
+                   console.error(err.stack);
+                   process.exit(1);
+                 });
+}
+
+// Connect to database
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+    insertSeeds();
+  })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
+
+  //to run file:
+  // dotenv node seeders/seeds.js
