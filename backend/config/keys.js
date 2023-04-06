897f6f5907497cd0d72029aeec638c6ba2a2de47
Julian Cardone <juliancardone1@gmail.com>
2023-04-05 22:20:44 -0400

    refactor backend

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -2,6 +2,5 @@ module.exports = {
   secretOrKey: process.env.SECRET_OR_KEY,
   mongoURI: process.env.MONGO_URI,
   googleAuth: process.env.GOOGLE_AUTH_API,
-  isProduction: process.env.NODE_ENV === 'production'
-}
- 
+  isProduction: process.env.NODE_ENV === "production",
+};
