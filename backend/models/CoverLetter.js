6fd0bf99cbb13d38478a167874fd5d80f36c3f05
Julian Cardone <juliancardone1@gmail.com>
2023-04-06 23:24:19 -0400

    refactor backend

--- backend/models/CoverLetter.js
+++ backend/models/CoverLetter.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Define schema for the document
 const coverLetterSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true }, // foreign key for user document belongs to
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, // foreign key for user document belongs to
   title: { type: String, required: true },
   file: { type: String, required: true },
 });
 
 // Define the collection and schema in the database
-const CoverLetter = mongoose.model('CoverLetter', coverLetterSchema);
+const CoverLetter = mongoose.model("CoverLetter", coverLetterSchema);
 
 module.exports = CoverLetter;
