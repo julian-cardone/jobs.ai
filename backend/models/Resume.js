5349a33280e5d61ffbb513c6440ab93882c1c80b
Julian Cardone <juliancardone1@gmail.com>
2023-04-07 20:54:33 -0400

    refactor backend

--- backend/models/Resume.js
+++ backend/models/Resume.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
- 
-const ResumeSchema = new Schema({
-  title: {
-    type: String,
-    required: false
-  },
-  userId: {
-    type: Schema.Types.ObjectId, ref: 'User',
-    required: true,
-    index: true
-  },
-  file: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-})
 
-module.exports = Resume = mongoose.model('Resume', ResumeSchema);
+const ResumeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    file: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Resume = mongoose.model("Resume", ResumeSchema);
