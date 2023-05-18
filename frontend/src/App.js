60c860b771ea0048d7729d71b684aa9c2861f30b
Julian Cardone <juliancardone1@gmail.com>
2023-05-17 21:40:11 -0400

    cover letter page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import LoginForm from "./components/SessionForms/LoginForm";
 import SignupForm from "./components/SessionForms/SignupForm";
 
 import { getCurrentUser } from "./store/session";
+import Home from "./components/HomePage";
+import CoverLetter from "./components/CoverLetter";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -24,9 +26,11 @@ function App() {
       <>
         <NavBar />
         <Switch>
+          <AuthRoute exact path="/" component={Home} />
           <AuthRoute exact path="/bs" component={BootstrapRef} />
           <AuthRoute exact path="/login" component={LoginForm} />
           <AuthRoute exact path="/signup" component={SignupForm} />
+          <ProtectedRoute exact path="coverletter" component={CoverLetter} />
         </Switch>
       </>
     )
---
+++ frontend/src/components/CoverLetter/index.js
@@ -0,0 +1,5 @@
+function CoverLetter(){
+
+}
+
+export default CoverLetter;
