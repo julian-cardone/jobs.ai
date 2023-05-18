f0be89cbffbebe41d511e43da83c0907344b3526
Julian Cardone <juliancardone1@gmail.com>
2023-05-18 14:48:02 -0400

    files can be added to aws

--- frontend/src/components/CoverLetter/index.js
+++ frontend/src/components/CoverLetter/index.js
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { newCoverLetter } from "../../store/coverLetter";
 
 function CoverLetter() {
 
   // const [name, setName] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    
-  }, []);
+  const handleSubmit = (e) => {
+    let files = e.target.files
+
+    let reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+
+    reader.onload= (e) =>{
+      console.warn("data ", e.target.result);
+
+      const formData={file:e.target.result}
+      dispatch(newCoverLetter(formData));
+    }
+
+  }
 
   return (
     <>
       <div className="container-lg my-5 pt-5">
         <div className="row">
           <div className="col-sm-6 col-md-6 col-lg-6">
-            <div class="mb-3">
-              <label for="formFile" class="form-label">
+            <form class="mb-3">
+              <label htmlFor="formFile" className="form-label">
                 Default file input example
               </label>
-              <input class="form-control" type="file" id="formFile" onChange={(e)=> setSelectedFile(e.target.files[0])}/>
-            </div>
+              <input className="form-control" type="file" id="formFile" onChange={(e)=> handleSubmit(e)}/>
+              {/* <input type="submit"></input> */}
+            </form>
           </div>
           {/* <div className="col-sm-4 col-lg-3">
             <div className="p-5 bg-primary text-light">col 2</div>
