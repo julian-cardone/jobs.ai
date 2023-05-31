49aeff7152e27b8817437a80088f3def04c639cb
Julian Cardone <juliancardone1@gmail.com>
2023-05-30 20:08:57 -0400

    figure out the parsing

--- frontend/src/components/CoverLetterUploaded/FileInput.js
+++ frontend/src/components/CoverLetterUploaded/FileInput.js
@@ -1,15 +1,27 @@
+import InputData from "../../utils/base64Decoder";
+
 function FileInput({ user, setSelectedLetter }) {
 
   const handleSubmit = async (e) => {
+    
     let files = e.target.files;
 
+
+
+
+    //pdf parser
+    let parser = new InputData();
+    
     let reader = new FileReader();
+    // console.log(reader.readAsDataURL);
+    
+    // reader.readAsDataURL(files[0]);
     reader.readAsDataURL(files[0]);
-
+    
     reader.onload = (e) => {
-
+      
       const formData = { file: e.target.result };
-      console.log(formData.file);
+      parser.decodeBase64(formData.file);
     
       // jwtFetch("/api/coverletter/upload", {
       //   method: "POST",
@@ -23,6 +35,13 @@ function FileInput({ user, setSelectedLetter }) {
       //   .then((res) => res.json())
       //   .then((data) => setSelectedLetter(data));
     };
+
+
+
+
+
+    //if size > 50k, type != application/pdf, etc...
+
   };
 
   return (
