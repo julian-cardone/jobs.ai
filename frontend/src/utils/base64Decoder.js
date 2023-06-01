145f476e707dc639a66ceba6f50a59b1d0947c8d
Julian Cardone <juliancardone1@gmail.com>
2023-05-31 20:03:08 -0400

    work on decoder

--- frontend/src/utils/base64Decoder.js
+++ frontend/src/utils/base64Decoder.js
@@ -1,25 +1,19 @@
 module.exports = class InputData {
-
   // constructor(data){
   //   this.data = data;
   // }
 
-  static map = {"A": "000000"};
+  static map = { A: "000000" };
 
-  decodeBase64(data){
-
+  decodeBase64(data) {
     let length = data.length / 6;
 
     // for (let i = 0; i <= length; i = i+6){
     //   console.log(data.slice(i, i + 6))
     // }
 
-    for (let i = 0; i < data.length; i++){
-      
-    }
-
+    for (let i = 0; i < data.length; i++) {}
 
     // console.log(data.slice(28));
   }
-
-}
+};
