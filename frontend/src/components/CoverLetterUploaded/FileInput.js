9187bfbc329ea84227458cb090b8d97d60acde3b
Julian Cardone <juliancardone1@gmail.com>
2023-06-04 23:18:33 -0400

    pdf parser

--- frontend/src/components/CoverLetterUploaded/FileInput.js
+++ frontend/src/components/CoverLetterUploaded/FileInput.js
@@ -1,9 +1,8 @@
 import InputData from "../../utils/base64Decoder";
-import  { createRef } from 'react';
+import { createRef } from "react";
 import jwtFetch from "../../store/jwt";
 
 function FileInput({ user, setSelectedLetter }) {
-
   // const fileInput = createRef();
 
   const handleSubmit = async (e) => {
@@ -14,7 +13,7 @@ function FileInput({ user, setSelectedLetter }) {
     //pdf parser
 
     // let reader = new FileReader();
-    
+
     // reader.readAsDataURL(file);
 
     //formdata method
@@ -25,50 +24,45 @@ function FileInput({ user, setSelectedLetter }) {
     //     for(var pair of formData.entries()) {
     //   console.log(pair[0]+', '+pair[1]);
     // }
-    
+
     // reader.onload = (e) => {
-      
-      // const formData = { file: e.target.result };
-      // parser.decodeBase64(formData.file);
 
-        jwtFetch("/api/coverletter/upload", {
-          method: "POST",
-          headers: { "Content-Type": "multipart/form-data" },
-          body: formData
-        });
+    // const formData = { file: e.target.result };
+    // parser.decodeBase64(formData.file);
 
-      //   const parseResponse = await response.json();
-      //   if (response.ok){
-      //     console.log("file uploaded")
-      //   } else {
-      //     console.error('error!!!')
-      //   }
-      // } catch (e) {
-      //   console.error(e.message)
-      // }
-        // .then((res) => res.text())
-        // .then((data) => console.log(data));
-    
-      // jwtFetch("/api/coverletter/upload", {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/json" },
-      //   files: formData,
-      //   body: JSON.stringify({
-      //     file: formData,
-      //     userId: user._id,
-      //     name: file.name,
-      //   }),
-      // })
-      //   .then((res) => res.json())
-      //   .then((data) => setSelectedLetter(data));
+    jwtFetch("/api/coverletter/upload", {
+      method: "POST",
+      headers: { "Content-Type": "multipart/form-data" },
+      body: formData,
+    });
+
+    //   const parseResponse = await response.json();
+    //   if (response.ok){
+    //     console.log("file uploaded")
+    //   } else {
+    //     console.error('error!!!')
+    //   }
+    // } catch (e) {
+    //   console.error(e.message)
+    // }
+    // .then((res) => res.text())
+    // .then((data) => console.log(data));
+
+    // jwtFetch("/api/coverletter/upload", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   files: formData,
+    //   body: JSON.stringify({
+    //     file: formData,
+    //     userId: user._id,
+    //     name: file.name,
+    //   }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => setSelectedLetter(data));
     // };
 
-
-
-
-
     //if size > 50k, type != application/pdf, etc...
-
   };
 
   return (
