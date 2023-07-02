4081e2278ebfbfec28efa6f1fecc04e4852c4a43
Julian Cardone <juliancardone1@gmail.com>
2023-07-02 14:58:22 -0400

    fix the worker for pdf.js

--- frontend/src/components/CoverLetterUploaded/FileInput.js
+++ frontend/src/components/CoverLetterUploaded/FileInput.js
@@ -1,29 +1,39 @@
-function FileInput({ user, setSelectedLetter }) {
+import React from 'react';
+import * as pdfjsLib from 'pdfjs-dist';
 
-  const handleSubmit = async (e) => {
-    let files = e.target.files;
+// pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.8.162/pdf.worker.js`;
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+
+function FileInput({ user, setSelectedLetter }) {
+  const handleSubmit = (e) => {
+    let file = e.target.files[0];
 
     let reader = new FileReader();
-    reader.readAsDataURL(files[0]);
-
-    reader.onload = (e) => {
-
-      const formData = { file: e.target.result };
-      console.log(formData.file);
-    
-      // jwtFetch("/api/coverletter/upload", {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/json" },
-      //   body: JSON.stringify({
-      //     file: formData,
-      //     userId: user._id,
-      //     name: files[0].name,
-      //   }),
-      // })
-      //   .then((res) => res.json())
-      //   .then((data) => setSelectedLetter(data));
+      reader.onload = function (e) {
+        const arrayBuffer = e.target.result;
+  
+        pdfjsLib.getDocument(arrayBuffer).promise.then(function (pdf) {
+          // Access PDF document and perform parsing or other operations here
+          console.log('PDF loaded:', pdf);
+        });
+      };
+  
+      reader.readAsArrayBuffer(file);
     };
-  };
+
+//read as dataurl
+  // jwtFetch("/api/coverletter/upload", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({
+  //     file: formData,
+  //     userId: user._id,
+  //     name: files[0].name,
+  //   }),
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => setSelectedLetter(data));
 
   return (
     <>
