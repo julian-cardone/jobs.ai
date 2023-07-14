24762c6145f2c3e165db2ec435bb18afaa7cf3d5
Julian Cardone <juliancardone1@gmail.com>
2023-07-14 15:48:30 -0400

    look into solutions to minimize davinci's role

--- backend/openai/openai.js
+++ backend/openai/openai.js
@@ -1,27 +1,31 @@
-const axios = require('axios');
+const axios = require("axios");
 const keys = require("../config/keys");
 
 async function generateChatCompletion(prompt) {
-  const url = 'https://api.openai.com/v1/completions';
+  const url = "https://api.openai.com/v1/completions";
   const model = "";
 
   try {
-    const response = await axios.post(url, {
-      model: model,
-      prompt: prompt,
-      max_tokens: 3000, // Adjust the number of tokens based on your needs
-      temperature: 0.7, // Adjust the temperature based on your needs
-      n: 1, // Adjust the number of completions you want
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${keys.openai}`,
+    const response = await axios.post(
+      url,
+      {
+        model: model,
+        prompt: prompt,
+        max_tokens: 3000, // Adjust the number of tokens based on your needs
+        temperature: 0.7, // Adjust the temperature based on your needs
+        n: 1, // Adjust the number of completions you want
       },
-    });
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${keys.openai}`,
+        },
+      }
+    );
 
     return response.data.choices[0].text.trim();
   } catch (error) {
-    console.error('OpenAI API request failed:', error.response.data);
+    console.error("OpenAI API request failed:", error.response.data);
     throw error;
   }
 }
